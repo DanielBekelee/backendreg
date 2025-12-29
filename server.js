64bb857eca78e756886ab0b437393ab1fb749c8c
DanielBekelee <daniel.bekele.garedew@gmail.com>
2025-12-29 11:07:35 +0300

    ff

--- server.js
+++ server.js
@@ -102,7 +102,8 @@ app.post("/login", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
